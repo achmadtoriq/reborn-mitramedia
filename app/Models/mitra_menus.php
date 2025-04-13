7370bc4baea18bc963b4df819d016173cd16038c
Ahmad Toriq Afandi <ahmad.toriq@corp.bm.co.id>
2025-04-13 23:24:38 +0700

    wwww

--- app/Models/mitra_menus.php
+++ app/Models/mitra_menus.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class mitra_menus extends Model
 {
-    protected $table = 'mitra_menuses';
+    protected $table = 'mitra_menus';
 }
--- database/migrations/2025_03_15_165707_create_mitra_menuses_table.php
+++ database/migrations/2025_03_15_165707_create_mitra_menuses_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('mitra_menuses', function (Blueprint $table) {
+        Schema::create('mitra_menus', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('classname');
